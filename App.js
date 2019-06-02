d00fc3e23009307bf3c8c4c12888f74f3ad1b6ca
HONGSIK <thescripts70@gmail.com>
2019-06-02 19:59:34 +0900

    feat: add redux store

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from "react";
 import { AppLoading, Asset, Font } from "expo";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+
+import configureStore from "./redux/configureStore";
+const { persistor, store } = configureStore();
 
 export default class App extends React.Component {
   state = {
@@ -11,26 +16,34 @@ export default class App extends React.Component {
     const { isLoading } = this.state;
 
     if (!isLoading) {
-      return <AppLoading 
-      startAsync={this._loadAssetsAsync}
-      onError={this._handleLoadingError}
-      onFinish={this._handleFinishLoading}
-      />;
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
     }
 
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text>Hello World!</Text>
+      // </View>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <View style={StyleSheet.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+          </View>
+        </PersistGate>
+      </Provider>
     );
   }
-
 
   _loadAssetsAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
         require("./assets/images/logo.png"),
-        // require("./assets/images/logo-white.png"),
+        require("./assets/images/logo-white.png"),
         require("./assets/images/noPhoto.jpg"),
         require("./assets/images/photoPlaceholder.png")
       ]),
@@ -44,7 +57,7 @@ export default class App extends React.Component {
     console.error(error);
   };
   _handleFinishLoading = async () => {
-    console.log('handle finish success.');
+    console.log("handle finish success.");
     this.setState({
       isLoading: true
     });
